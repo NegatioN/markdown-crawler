8dc2d443cd76afd5e529bdd5275345088f87ffcd
NegatioN <4037769+NegatioN@users.noreply.github.com>
2025-11-30 10:08:24 +0100

    install dependencies on install

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name="markdown-crawler",
     packages=find_packages(exclude=['markdown']),
     include_package_data=True,
+    install_requires=install_requires,
 )
